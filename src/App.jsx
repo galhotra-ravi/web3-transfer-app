0f57eb0bc96b69fa226b97544da8e7da68a0e1c7
galhotra-ravi <ravigalhotra007@gmail.com>
2024-10-07 21:27:42 +0530

    wallet connect feature added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,53 @@
-import { useWeb3React } from "@web3-react/core";
-import { InjectedConnector } from "@web3-react/injected-connector";
-
-const injected = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5, 42], // Mainnet and testnets
-});
+import React, { useState } from "react";
+import { BrowserProvider } from 'ethers';
+  import Web3Modal from 'web3modal';
+  import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-  const { activate, account } = useWeb3React();
-
-  const connectWallet = () => {
-    activate(injected);
+  
+  const notifyNoWeb3Wallet = () => {
+    toast.error(('No web3 wallet detected!'), {
+      toastId: 1
+    });
+  }
+  const notifyWalletConnected = () => {
+    toast.success(('Wallet connected successfully!'), {
+      toastId: 2
+    });
+  }
+  
+  const [isWalletConnected, setisWalletConnected] = useState(false)
+  const [connectedAddress, setConnectedAddress] = useState('')
+  
+  const connectWallet = async () => {
+    try {
+      if (typeof window.ethereum === 'undefined') {
+        notifyNoWeb3Wallet()
+        return;
+      }
+  
+      const web3Modal = new Web3Modal({
+        cacheProvider: false,
+        providerOptions: {},
+      });
+  
+      const instance = await web3Modal.connect();
+      const provider = new BrowserProvider(instance);
+      const signer = await provider.getSigner();
+      console.log('Connected Wallet Address:', await signer.getAddress());
+      setisWalletConnected(true)
+      setConnectedAddress((await signer.getAddress()).toString())
+      notifyWalletConnected()
+    } catch (error) {
+      console.error('Error connecting wallet:', error);
+      setisWalletConnected(false)
+    }
   };
+
   return (
     <>
-      {/* <div className="w-full h-screen flex justify-center items-center">
+      <div className="w-full h-screen flex justify-center items-center">
         <div className="w-fit min-w-72 h-fit p-5  rounded-xl border-[1px] border-[#a855f7]  shadow-sm shadow-[#a855f7] hover:shadow-md transition-shadow duration-300 flex flex-col gap-5">
 
 
@@ -25,9 +59,9 @@ const App = () => {
           </div>
 
           <div >
-            <button className="h-fit w-full py-2 rounded-md flex justify-center items-center gap-2 text-base text-[#111827] bg-white hover:opacity-85 transition-all ease-in-out duration-150" >
-            <i class="fa-solid fa-wallet mt-[2px]"></i>
-            <p className="font-semibold">Connect Wallet</p>
+            <button onClick={connectWallet} className="h-fit w-full py-2 rounded-md flex justify-center items-center gap-2 text-base text-[#111827] bg-white hover:opacity-85 transition-all ease-in-out duration-150" >
+            <i className="fa-solid fa-wallet mt-[2px]"></i>
+            <p className={`font-semibold ${isWalletConnected ? 'text-[#a855f7]' : ''}`}> {isWalletConnected ? `${connectedAddress.substring(0, 3)}...${connectedAddress.substring(connectedAddress.length-3, connectedAddress.length)} connected` : 'Connect Wallet'} </p>
             </button>
           </div>
 
@@ -41,19 +75,13 @@ const App = () => {
 
           <div >
             <button className="h-fit w-full py-2 rounded-md flex justify-center items-center gap-2 text-base text-[#ffffff] bg-[#a855f7] hover:opacity-85 transition-all ease-in-out duration-150" >
-            <i class="fa-solid fa-paper-plane mt-[2px] "></i>
+            <i className="fa-solid fa-paper-plane mt-[2px] "></i>
             <p className="font-semibold">Transfer</p>
             </button>
           </div>
 
         </div>
-      </div> */}
-      <div>
-        {account ? (
-          <p>Connected: {account}</p>
-        ) : (
-          <button onClick={connectWallet}>Connect MetaMask</button>
-        )}
+        <ToastContainer />
       </div>
     </>
   );
